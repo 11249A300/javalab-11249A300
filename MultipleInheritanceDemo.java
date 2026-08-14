326b1bb8be3263d467e97aad1df24947b97a7429
PUJARI HEMESH <11249a300@kanchiuniv.ac.in>
2026-08-14 10:05:15 +0530

    Implement class C with multiple inheritance
    Added class C implementing interfaces A and B with methods to display messages.

--- MultipleInheritanceDemo.java
+++ MultipleInheritanceDemo.java
@@ -1,72 +1,19 @@
-import java.util.Scanner;
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+class C implements A, B {
+   C() {
+   }
 
-interface Printable {
-    void printDetails();
-}
+   public void showA() {
+      System.out.println("This is interface:A");
+   }
 
-interface Showable {
-    void showGrade();
-}
+   public void showB() {
+      System.out.println("This is interface B");
+   }
 
-class StudentResult implements Printable, Showable {
-    int rollNo;
-    String name;
-    int total;
-    double average;
-
-    StudentResult(int rollNo, String name, int total, double average) {
-        this.rollNo = rollNo;
-        this.name = name;
-        this.total = total;
-        this.average = average;
-    }
-
-    public void printDetails() {
-        System.out.println("Roll No : " + rollNo);
-        System.out.println("Name    : " + name);
-        System.out.println("Total   : " + total);
-        System.out.println("Average : " + average);
-    }
-
-    public void showGrade() {
-        if (average >= 90)
-            System.out.println("Grade   : A+");
-        else if (average >= 80)
-            System.out.println("Grade   : A");
-        else if (average >= 70)
-            System.out.println("Grade   : B");
-        else if (average >= 60)
-            System.out.println("Grade   : C");
-        else if (average >= 50)
-            System.out.println("Grade   : D");
-        else
-            System.out.println("Grade   : F");
-    }
-}
-
-public class MultipleInheritanceDemo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Roll No: ");
-        int rollNo = sc.nextInt();
-        sc.nextLine();
-
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Enter Total Marks: ");
-        int total = sc.nextInt();
-
-        System.out.print("Enter Average: ");
-        double average = sc.nextDouble();
-
-        StudentResult sr = new StudentResult(rollNo, name, total, average);
-
-        System.out.println("\nStudent Details:");
-        sr.printDetails();
-        sr.showGrade();
-
-        sc.close();
-    }
+   public static void main(String[] var0) {
+      C var1 = new C();
+      var1.showA();
+      var1.showB();
+   }
 }
